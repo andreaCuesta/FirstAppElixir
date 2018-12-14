29abdda6ec2a9a1583ec17161ec06932bfd4df8e
Andrea Cuesta <andrea@kommit.co>
2018-12-14 11:56:54 -0500

    Apply try, throw and catch for handling impure functions

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -3,6 +3,8 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
     alias Mix.Shell.IO, as: Shell
     
+    @invalid_option {:error, "Invalid option"}
+    
     def display_options(options) do
         options
         |> Enum.with_index(1)
@@ -18,8 +20,36 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     end
     
     def parse_answer(answer) do
-        {option, _} = Integer.parse(answer)
-        option - 1
+        case Integer.parse(answer) do
+        :error ->
+            throw @invalid_option
+        {option, _} ->
+            option - 1
+        end
+    end
+    
+    def find_option_by_index(index, options) do
+        Enum.at(options, index) || throw @invalid_option
+    end
+    
+    def ask_for_option(options) do
+            options
+            |> display_options
+            |> generate_question
+            |> Shell.prompt
+            |> parse_answer
+            |> find_option_by_index(options)
+        catch
+            {:error, message} ->
+                display_error(message)
+                ask_for_option(options)
+    end
+    
+    def display_error(message) do
+        Shell.cmd("clear")
+        Shell.error(message)
+        Shell.prompt("Press Enter to continue.")
+        Shell.cmd("clear")
     end
 end
 
@@ -37,3 +67,20 @@ end
 # parse_answer function
 # It tries to parse an integer from the user input, then subtracts one to get the index # of the hero
 
+# find_option_by_index function
+# It get the corresponding option according to the index. If Enum.at(options, index) 
+# returns a falsy value it throw @invalid_option. Remember that nil
+# is a falsy value
+
+# "@invalid_option" -> contains an atom indicating an error, and a string with an error
+# message. Then we used the function throw to stop the function execution from throwing
+# the @invalid_option value when parse_answer/1 or find_option_by_index/2 results in an
+# error
+
+# ask_for_option function
+# use different functions that helps it to ask the user for an option, between many 
+# posibilities and manage the answer, at the end return the option selected. 
+# Inside the try block(try was omitted because we only need one try) we have the happy-
+# path code. In the catch block we used pattern matching to catch the values thrown.
+# catch works very similarly to the case statement: each line has a pattern-matching
+# expression and a code block to be executed.
